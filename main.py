8e890f19dd4bbf39b72e1787cae8cd87bffb1c9d
bigvik <bigvikt@gmail.com>
2022-09-26 16:29:38 +0600

    Its work

---
+++ main.py
@@ -0,0 +1,99 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from spotipy.exceptions import SpotifyException
+from yandex_music import Client
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
+
+
+CLIENT_ID = '3d72b7b2090f4c41b74157954784a4ad'
+CLIENT_SECRET = '909e321fe43e4f3e87af732859e5c8d0'
+REDIRECT_URI = 'http://localhost:8888/callback'
+SCOPE = 'user-library-modify playlist-modify-public playlist-modify-private'
+TOKEN = 'y0_AgAAAAAAZ-9eAAG8XgAAAADMw4n2HbuHYtVVRwWYvHHPwJ2T1GiUkzM'
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+class NotFoundException(SpotifyException):
+    def __init__(self, item_name):
+        self.item_name = item_name
+
+
+class Fytos:
+    def __init__(self):
+        self.client_id = CLIENT_ID
+        self.client_secret = CLIENT_SECRET
+        self.redirect_uri = REDIRECT_URI
+        self.scope = SCOPE
+        self.token = TOKEN
+        self.spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(
+            client_id=self.client_id,
+            client_secret=self.client_secret,
+            scope=self.scope,
+            redirect_uri=self.redirect_uri
+        ))
+        self.spotify_id = self.spotify.me()['id']
+        self.yandex = Client(self.token).init()
+        logger.info('Initialization is OK')
+        logger.info(f'Spotify: {self.spotify_id}')
+        logger.info(f'Yandex: {self.yandex.me["account"]["login"]}')
+
+    def create_playlist(self, playlist_name, playlist_desc):
+        playlist = self.spotify.user_playlist_create(
+            user=self.spotify_id,
+            name=playlist_name,
+            public=True,
+            description=playlist_desc
+        )
+        return playlist['uri']
+
+    def found_track(self, track):
+        track_name = f'{", ".join([artist.name for artist in track.artists])} - {track.title}'
+        query = track_name.replace('- ', '')
+        founds = self.spotify.search(query, type='track')
+        if not len(founds):
+            raise NotFoundException(track_name)
+        logger.info(f'{track_name} ID: {founds["tracks"]["items"][0]["id"]}')
+        return founds['tracks']['items'][0]['id']
+
+    def collect_tracks(self):
+        collection = []
+        tracks = self.get_liked_tracks()
+        for track in tracks:
+            try:
+                found = self.found_track(track)
+                collection.append(found)
+            except Exception as exception:
+                logger.warning(f'Not found: {exception}')
+                continue
+        logger.info(f'Collected: {len(collection)} songs')
+        playlist = self.create_playlist('Yandex imported', f'Imported from Yandex Music {len(collection)} songs')
+        logger.info('Adding tracks into liked...')
+        for chunk in chunks(collection, 50):
+            self.spotify.current_user_saved_tracks_add(chunk)
+            self.spotify.playlist_add_items(playlist, chunk)
+        logger.info('OK')
+        #logger.info(f'Adding tracks into playlist Yandex Imported...')
+        #self.spotify.playlist_add_items(playlist, collection)
+        #logger.info('OK')
+        logger.info('Your are Welcome!')
+
+    def get_liked_tracks(self):
+        likes_tracks = self.yandex.users_likes_tracks().tracks
+        tracks = self.yandex.tracks([f'{track.id}:{track.album_id}' for track in likes_tracks if track.album_id])
+        return tracks
+
+
+if __name__ == '__main__':
+    fytos = Fytos()
+    fytos.collect_tracks()
